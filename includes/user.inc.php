662952fdd28413568852a502e3d13496cf98fe17
hwm1 <kiosk138@excite.com>
2018-11-24 17:05:45 -0500

    Add files via upload

--- includes/user.inc.php
+++ includes/user.inc.php
@@ -14,11 +14,17 @@ class User extends Dbh
     public function makeUser($connection)
     {
 
-        $this->first = mysqli_real_escape_string($connection, $_POST['first']);
-        $this->last = mysqli_real_escape_string($connection, $_POST['last']);
-        $this->email = mysqli_real_escape_string($connection, $_POST['email']);
-        $this->uid = mysqli_real_escape_string($connection, $_POST['uid']);
-        $this->pwd = mysqli_real_escape_string($connection, $_POST['pwd']);
+        // $this->first = mysqli_real_escape_string($connection, $_POST['first']);
+        // $this->last = mysqli_real_escape_string($connection, $_POST['last']);
+        // $this->email = mysqli_real_escape_string($connection, $_POST['email']);
+        // $this->uid = mysqli_real_escape_string($connection, $_POST['uid']);
+        // $this->pwd = mysqli_real_escape_string($connection, $_POST['pwd']);
+
+        $this->first = $_POST['first'];
+        $this->last = $_POST['last'];
+        $this->email = $_POST['email'];
+        $this->uid = $_POST['uid'];
+        $this->pwd = $_POST['pwd'];
 
         //error handlers
         //check for empty fields
@@ -42,29 +48,52 @@ class User extends Dbh
         }
 
         //see if have users with user name (uid)
-        $sql = "SELECT * FROM users WHERE user_uid='$this->uid'";
-        $result = mysqli_query($connection, $sql);
-        $resultCheck = mysqli_num_rows($result);
+        // $sql = "SELECT * FROM users WHERE user_uid='$this->uid'";
+        // $result = mysqli_query($connection, $sql);
+        // $resultCheck = mysqli_num_rows($result);
+        // if ($resultCheck > 0) {
+        //     header("Location: ../signup.php?signup=usertaken");
+        //     exit();
+        // }
+        $stmt = $connection->prepare("select user_uid from users where user_uid=?");
+        $stmt->bind_param("s", $this->uid);
+        $stmt->execute();
+        $stmt->bind_result($resultCheck);
+        $stmt->fetch();
+
         if ($resultCheck > 0) {
             header("Location: ../signup.php?signup=usertaken");
             exit();
         }
         //hash the password
         $this->hashedPwd = password_hash($this->pwd, PASSWORD_DEFAULT);
-        //insert the user into the database
-        $sql = "INSERT INTO users(user_first, user_last,
-                        user_email, user_uid, user_pwd) VALUES('$this->first','$this->last',
-                        '$this->email','$this->uid','$this->hashedPwd');";
 
-        return $sql;
+        //insert the user into the database
+
+        // $sql = "INSERT INTO users(user_first, user_last,
+        //                 user_email, user_uid, user_pwd) VALUES('$this->first','$this->last',
+        //                 '$this->email','$this->uid','$this->hashedPwd');";
+
+        $stmt = $connection->prepare("INSERT INTO users(user_first, user_last,
+        user_email, user_uid, user_pwd) VALUES(?,?,?,?,?)");
+
+        $stmt->bind_param("sssss", $this->first, $this->last,
+            $this->email, $this->uid, $this->hashedPwd);
+
+        $stmt->execute();
+
+        //      return $sql;
 
     }
 
     public function findUser($connection)
     {
 
-        $this->uid = mysqli_real_escape_string($connection, $_POST['uid']);
-        $this->pwd = mysqli_real_escape_string($connection, $_POST['pwd']);
+        // $this->uid = mysqli_real_escape_string($connection, $_POST['uid']);
+        // $this->pwd = mysqli_real_escape_string($connection, $_POST['pwd']);
+        $this->uid = $_POST['uid'];
+        $this->pwd = $_POST['pwd'];
+
         //error handlers
         //check if inputs are empty
 
@@ -72,15 +101,32 @@ class User extends Dbh
             header("Location: ../index.php?login=empty");
             exit();
         }
-        $sql = "SELECT * FROM users WHERE user_uid='$this->uid' OR user_email='$this->uid'";
-        $result = mysqli_query($connection, $sql);
-        $resultCheck = mysqli_num_rows($result);
+        // $sql = "SELECT * FROM users WHERE user_uid='$this->uid' OR user_email='$this->uid'";
+        // $result = mysqli_query($connection, $sql);
+        // $resultCheck = mysqli_num_rows($result);
 
-        if ($resultCheck < 1) { //user not found in db
-            header("Location: ../index.php?login=error");
+        $stmt = $connection->prepare("SELECT * FROM users WHERE user_uid=? OR user_email=?");
+        $stmt->bind_param("ss", $this->uid, $this->uid);
+        $stmt->execute();
+
+        //   $result =     $stmt->store_result();
+        $result = $stmt->get_result();
+
+        // if ($resultCheck < 1) { //user not found in db
+        //     header("Location: ../index.php?login=error");
+        //     exit();
+        // }
+
+        //    echo($result->num_rows);
+
+        if ($result->num_rows < 1) { //user not found in db
+            header("Location: ../index.php?login=not_found");
             exit();
         }
-        if ($row = mysqli_fetch_assoc($result)) {
+
+        // if ($row = mysqli_fetch_assoc($result)) {
+        if ($row = $result->fetch_assoc()) {
+
             //dehash pwd
             $hashedPwdCheck = password_verify($this->pwd, $row['user_pwd']);
 
@@ -99,7 +145,5 @@ class User extends Dbh
                 exit();
             }
         }
-
     }
-
 }
